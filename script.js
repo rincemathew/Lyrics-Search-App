688ac32e790a00a56259e458751524b59dbeb577
Rince Mathew <rincemathewmanattu@gmail.com>
2022-11-12 15:30:12 +0530

    get and disokay song lyrics

--- script.js
+++ script.js
@@ -61,6 +61,20 @@ async function getMoreSongs(url) {
 }
 
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = '';
+}
+
+
 // Event listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -73,3 +87,16 @@ form.addEventListener('submit', e => {
       searchSongs(searchTerm);
     }
   });
+
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
